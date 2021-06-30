6e664dcb138bd39db82ef9a7eea5d4c9df500f15
pauld <developer.debu@gmail.com>
2021-07-01 01:39:37 +0530

    Part 1 push

--- Part1/index.php
+++ Part1/index.php
@@ -1,8 +1,39 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class class1 {
 
+    public $x = 1;
+
+    function fun1() {
+        return $this->x++;
+    }
+
+}
+
+$obj1 = new class1();
+$obj2 = new class1();
+
+$obj1->fun1();
+echo $obj1->x;
+
+echo "<br/>";
+
+echo $obj2->x;
+
+/* class class1{
+  function __construct($y){
+  $this->x=$y;
+  }
+
+  function fun1(){
+  echo $this->x;
+  }
+
+  function __destruct(){
+  echo "end";
+  }
+  }
+
+  $obj1=new class1(20);
+  $obj1->fun1(); */
+?>
